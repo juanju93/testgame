e121a87d7aeadd33305a72c4f2bf8a0bd5871576
juanju93 <juhoantti@hotmail.com>
2018-02-08 14:49:27 +0200

    joopajoo

--- Assets/Scripts/ObjectiveSpawner.cs
+++ Assets/Scripts/ObjectiveSpawner.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class ObjectiveSpawner : MonoBehaviour {
 
+	public GameObject player;
 	public GameObject token;
 	public GameObject[] trees;
+	public GameObject bigTree;
+	public GameObject[] debrees;
+
+
 	private float tokenTimer = 2.0f;
 	private float treeTimer = 2.0f;
+	private float bigTreeTimer = 5.0f;
+
+	public AudioClip bigTreeFalling;
 
 	// Update is called once per frame
 	void Update () {
 		tokenTimer -= Time.deltaTime;
 		treeTimer -= Time.deltaTime;
+		bigTreeTimer -= Time.deltaTime;
 		if (tokenTimer < 0) {
 			SpawnToken ();
 		}
 		if (treeTimer < 0) {
 			SpawnTree ();
+		}
+		if (bigTreeTimer < 0) {
+			StartCoroutine (SpawnBigTree ());
 		}
 	}
 
@@ -30,5 +42,30 @@ public class ObjectiveSpawner : MonoBehaviour {
 		GameObject tre = Instantiate (trees[(Random.Range(0, trees.Length))],new Vector2(Random.Range(-6, 6), 10),Quaternion.identity) as GameObject;
 		treeTimer = Random.Range(0.5f, 2.5f);
 		tre.GetComponent<Rigidbody2D> ().gravityScale = Random.Range (1, 3);
+	}
+
+	IEnumerator SpawnBigTree ()
+	{
+		bigTreeTimer = 1000;
+		Vector2 spawnSpot;
+		int debreeTimer = (Random.Range (20, 30));
+		int spwnOnPlayerPer = (Random.Range (0, 100));
+		if (spwnOnPlayerPer < 50) {
+			spawnSpot = new Vector2 (Random.Range (-6, 6), 20);
+		} else {
+			spawnSpot = new Vector2 (player.transform.position.x, 20);
+		}
+		while (debreeTimer > 0) {
+			debreeTimer--;
+			float t = Random.Range (0.005f, 0.1f);
+			yield return new WaitForSeconds (t);
+			Debug.Log ("Spawned");
+			GameObject debree = Instantiate (debrees [(Random.Range (0, debrees.Length))], new Vector2 (spawnSpot.x + Random.Range (-1.0f, 1.0f), spawnSpot.y - 10), Quaternion.identity) as GameObject;
+		}
+		Debug.Log ("Complete");
+		yield return new WaitForSeconds (1);
+		Instantiate (bigTree, spawnSpot, Quaternion.identity);
+		player.GetComponent<AudioSource> ().PlayOneShot (bigTreeFalling, 3.0f);
+		bigTreeTimer = Random.Range (15.0f, 45.0f);
 	}
 }
